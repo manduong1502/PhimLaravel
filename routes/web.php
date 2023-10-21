ea2937cee3a674547788703c8fa87eb9cec6aee1
manduong <minhmanuzu@gmail.com>
2023-10-21 13:47:18 +0700

    xong cac trang

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Pagecontroler;
 use Illuminate\Contracts\Pagination\Paginator;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/index', [Pagecontroler::class, 'getIndex']);
-Route::get('/chitiet', [Pagecontroler::class,'getLoaiSP']);
+Route::get('/', [Pagecontroler::class,'getGioithieu']);
+Route::get('/index', [Pagecontroler::class, 'getTrangchu']);
+Route::get('/chitiet', [Pagecontroler::class,'getChitiet']);
+Route::get('/xemphim', [Pagecontroler::class,'getXemphim']);
+Route::get('/theloai', [Pagecontroler::class,'getTheloai']);
+Route::get('/blog', [Pagecontroler::class,'getBlog']);
+Route::get('/blog_review', [Pagecontroler::class,'getBlog_review']);
+
+
