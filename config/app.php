61160dcb0e60b2a2dc383fb70d405a2ee3c9a2ef
manduong <minhmanuzu@gmail.com>
2023-10-24 01:04:46 +0700

    env

--- config/app.php
+++ config/app.php
@@ -184,7 +184,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-         'Socialite' =>Laravel\Socialite\Facades\Socialite::class,
     ])->toArray(),
 
 ];
