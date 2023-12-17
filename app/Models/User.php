54bb5a5ac510bb1fc086227679d26a6e8067d8c3
Vũ trần <levu1962004@gmail.com>
2023-12-18 02:42:23 +0700

    thêm tính năng login google facebook

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable
 {
@@ -24,6 +27,8 @@ class User extends Authenticatable
         'username',
         'email',
         'password',
+        'google_id',
+        'facebook_id',
     ];
 
     /**
